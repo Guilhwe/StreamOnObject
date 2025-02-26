f731465aeba96d3a98f26e01dff34db4b08fc12a
Guilherme Melo <guilherme96melo@gmail.com>
2025-02-26 17:51:50 +0100

    creacion de un metodo en la clase peli para que se pueda acceder desde fuera para que no se necesite escribir un codigo distinto cada vezs que se requiere la informacion de cada pelicula.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Peli pelicula1 = new Peli();
+        pelicula1.nombre = "Un monstruo viene a verme";
+        pelicula1.añoDeEstreno = 2016;
+        pelicula1.incluido = true;
+        pelicula1.nota = 9.5;
+        pelicula1.totalEvaluaciones = 1000;
+        pelicula1.duracionEnMinutos = 108;
+
+        pelicula1.info();
     }
 }
--- src/Peli.java
+++ src/Peli.java
@@ -5,4 +5,13 @@ public class Peli {
     double nota;
     int totalEvaluaciones;
     int duracionEnMinutos;
+
+    void info(){
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Año de estreno: " + añoDeEstreno);
+        System.out.println("Incluido en Netflix: " + incluido);
+        System.out.println("Nota: " + nota);
+        System.out.println("Total de evaluaciones: " + totalEvaluaciones);
+        System.out.println("Duración en minutos: " + duracionEnMinutos);
+    }
 }
