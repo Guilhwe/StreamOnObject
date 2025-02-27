e3db1d32aa18831e69df640c13b501e4777d871f
Guilherme Melo <guilherme96melo@gmail.com>
2025-02-27 18:51:36 +0100

    Uso de setters y getters para establecer que se puede hacer con las variables

--- src/App.java
+++ src/App.java
@@ -3,16 +3,16 @@ import es.com.Alura.PelisOn.modelos.Peli;
 public class App {
     public static void main(String[] args) throws Exception {
         Peli pelicula1 = new Peli();
-        pelicula1.nombre = "Un monstruo viene a verme";
-        pelicula1.añoDeEstreno = 2016;
-        pelicula1.incluido = true;
-        pelicula1.duracionEnMinutos = 108;
+        pelicula1.setNombre("Un monstruo viene a verme");
+        pelicula1.setAñoDeEstreno(2016);
+        pelicula1.getIncluido();
+        pelicula1.setDuracionEnMinutos(108);
 
         pelicula1.puntuar(8.5);
         pelicula1.puntuar(9.0);
         pelicula1.puntuar(7.5);
         pelicula1.puntuar(8.0);
-        System.out.println(pelicula1.gettotalEvaluaciones());
+        System.out.println(pelicula1.getTotalEvaluaciones());
         
     }
 }
--- src/es/com/Alura/PelisOn/modelos/Peli.java
+++ src/es/com/Alura/PelisOn/modelos/Peli.java
@@ -1,16 +1,32 @@
 package es.com.Alura.PelisOn.modelos;
 
 public class Peli {
-    public String nombre;
-    public int añoDeEstreno;
-    public boolean incluido;
+    private String nombre;
+    private int añoDeEstreno;
+    private boolean incluido;
     private double sumaNotas;
     private int totalEvaluaciones;
-    public int duracionEnMinutos;
+    private int duracionEnMinutos;
 
-    public int gettotalEvaluaciones() {
+    public int getTotalEvaluaciones() {
         return totalEvaluaciones;
     }
+    public boolean getIncluido() {
+        return incluido;
+    }
+    //this es una referencia a la variable local
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setAñoDeEstreno(int añoDeEstreno) {
+        this.añoDeEstreno = añoDeEstreno;
+    }
+    
+    public void setDuracionEnMinutos(int duracionEnMinutos) {
+        this.duracionEnMinutos = duracionEnMinutos;
+    }
+
 
     public void info(){
         System.out.println("Nombre: " + nombre);
