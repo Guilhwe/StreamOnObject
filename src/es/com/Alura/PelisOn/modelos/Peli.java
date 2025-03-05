2fda39dcd4a1a64ff13c60a9cafc9470a0b5c6a0
Guilherme Melo <guilherme96melo@gmail.com>
2025-03-05 17:08:54 +0100

    Uso de herencia. ahora tenenemois una superclase llamada Produccion que engloba la clase de peli la cual como atributo exclusvivo tiene el director solamente. asi los atributos de la clase Produccion podran ser utilizados tbn por una futurqa clase que herede de ella. */

--- src/es/com/Alura/PelisOn/modelos/Peli.java
+++ src/es/com/Alura/PelisOn/modelos/Peli.java
@@ -1,50 +1,15 @@
 package es.com.Alura.PelisOn.modelos;
 
-public class Peli {
-    private String nombre;
-    private int añoDeEstreno;
-    private boolean incluido;
-    private double sumaNotas;
-    private int totalEvaluaciones;
-    private int duracionEnMinutos;
+public class Peli extends Produccion{
+    private String director;
 
-    public int getTotalEvaluaciones() {
-        return totalEvaluaciones;
-    }
-    public boolean getIncluido() {
-        return incluido;
-    }
-    //this es una referencia a la variable local
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+    public void setDirector(String director) {
+         this.director = director;
+     }
 
-    public void setAñoDeEstreno(int añoDeEstreno) {
-        this.añoDeEstreno = añoDeEstreno;
-    }
-    
-    public void setDuracionEnMinutos(int duracionEnMinutos) {
-        this.duracionEnMinutos = duracionEnMinutos;
-    }
-
-
-    public void info(){
-        System.out.println("Nombre: " + nombre);
-        System.out.println("Año de estreno: " + añoDeEstreno);
-        System.out.println("Incluido en StreamOn: " + incluido);
-        System.out.println("Nota Media: " + notaMedia());
-        System.out.println("Total de evaluaciones: " + totalEvaluaciones);
-        System.out.println("Duración en minutos: " + duracionEnMinutos);
-    }
-
-    public void puntuar(double nota) {
-        sumaNotas += nota;
-        totalEvaluaciones++;
-    }
-
-    public double notaMedia() {
-        return sumaNotas / totalEvaluaciones;
-    }
+    public String getDirector() {
+            return director;
+        }
 
         
 }
